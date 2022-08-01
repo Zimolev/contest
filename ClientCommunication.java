a3c81eb7f5129ef1acf001d264687df5a561d978
Oleg Zimnik <79708122+Zimolev@users.noreply.github.com>
2022-08-01 09:44:26 +0400

    /client/comunication

---
+++ ClientCommunication.java
@@ -0,0 +1,33 @@
+package com.company.client.comunication;
+
+
+import java.io.IOException;
+
+public class ClientCommunication {
+    private final ClientConnector connector;
+
+    public ClientCommunication() {
+        connector = new ClientConnector();
+    }
+
+    public void sendMessage(String outboundMessage) {
+        try {
+            connector.getOut().writeUTF(outboundMessage);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String receiveMessage() {
+        try {
+           return connector.getIn().readUTF();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Что то пошло не так", e);
+        }
+    }
+
+
+
+}
